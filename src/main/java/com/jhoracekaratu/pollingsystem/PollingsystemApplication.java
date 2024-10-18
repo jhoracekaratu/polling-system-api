46b2a64ec6e107770456f6d8e231edf238d119b8
jhoracekaratu <jhorace.karatu@gmail.com>
2024-10-18 19:21:38 +0300

    project setup

---
+++ src/main/java/com/jhoracekaratu/pollingsystem/PollingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.jhoracekaratu.pollingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhoracekaratu/pollingsystem/PollingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhoracekaratu.pollingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
